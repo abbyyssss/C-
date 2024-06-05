a2e8264cd0c077010ec4c0366a7ade860a67cfa2
Kauan <kauan.mcvquaresma@senacsp.edu.br>
2024-06-05 16:58:04 -0300

    primeiras atividades em C#

---
+++ ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿int dividendo, divisor, quociente, resto;
+Console.WriteLine("digite o dividendo :");
+dividendo = int.Parse(Console.ReadLine());
+Console.WriteLine("escreva o divisor:");
+divisor = int.Parse(Console.ReadLine());
+
+quociente = dividendo / divisor;
+resto = dividendo % divisor;
+
+Console.WriteLine("seu dividendo é :" + dividendo +".");
+Console.WriteLine("seu divisor é :" + divisor + ".");
+Console.WriteLine(" seu quociente é :" + quociente + ".");
+Console.WriteLine(" o resto é :" +resto+ ".");
---
+++ ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Design;
+
+int idade;
+
+Console.WriteLine("Insira sua idade: ");
+idade = int.Parse(Console.ReadLine());
+
+if (idade < 16){
+    Console.WriteLine("Não eleitor(abaixo de 16 anos)");
+   } 
+else if (idade >= 18 && idade <= 65){
+        Console.WriteLine("Eleitor obrigatório ( entre 18 e 65 anos)");
+     }
+else if (idade > 65){
+    Console.WriteLine("Eleitor facultativo (maior do que 65 anos)");
+}
+       
+     
