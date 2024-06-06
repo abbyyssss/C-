104264d5703dcad17b255d33d03bcc4d8b4c10f9
KAUAN MATHEUS CARVALHO DO VALE QUARESMA <kauan.mcvquaresma@senacsp.edu.br>
2024-06-06 14:10:35 -0300

    testandno do while

---
+++ ex3- while/Do While 1/Do While 1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double tempC, tempF;
+char r;
+
+do
+{
+    Console.WriteLine("digite a temperatura em Celcius: ");
+    tempC = double.Parse(Console.ReadLine());
+
+    tempF = (tempC * 9.0 / 5.0) + 32.0;
+    Console.WriteLine(" a temperatura em fahrenheit é :" + tempF + ".");
+
+    Console.WriteLine(" deseja repetir? (s/n) : ");
+    r = char.Parse(Console.ReadLine());
+} while (r == 's' || r == 'S');
