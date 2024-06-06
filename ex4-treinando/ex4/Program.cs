1254fdb28f024aaa8fa3ba636843a97d346947d7
KAUAN MATHEUS CARVALHO DO VALE QUARESMA <kauan.mcvquaresma@senacsp.edu.br>
2024-06-06 15:53:40 -0300

    testando do while, if, etc.

---
+++ ex4-treinando/ex4/Program.cs
@@ -0,0 +1,8 @@
+﻿int num, i;
+Console.WriteLine("digite um número para ver sua tabuada :");
+num = int.Parse(Console.ReadLine());
+
+for (i = 1; i <= 10; i++)
+{
+    Console.WriteLine(num + " x " + i + "= " + num * i + ".");
+}
---
+++ ex5-desenho triangulo/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿int b, i, j;
+Console.WriteLine("digite a base do triangulo : ");
+b = int.Parse(Console.ReadLine());
+
+for (i = 0; i < b; i++)
+{
+    for (j = 0; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
